26a96ea15b2eab16b667af6865b19c88424fd5cd
Michael Lombard <CaptainRedBeard@users.noreply.github.com>
2016-03-13 17:04:40 -0500

    Update function
    Adding my solution to the problem.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion programming assignment 3/13/16
 
-## Write a short comment describing this function
+
+## Generates a list of the values of a matrix and the inverse of that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_mat <- NULL
+        change_mat <- function(y){
+        x <<- y
+        inv_mat <<- NULL
+        }
+        main_mat <- function() x
+        startInverse <- function(inverse) inv_mat <<- inverse
+        returnInverse <- function() inv_mat
+        list(change_mat = change_mat, 
+                main_mat = main_mat, 
+                startInverse = startInverse, 
+                returnInverse = returnInverse)
+        
 }
 
 
-## Write a short comment describing this function
+## Calculates if inverse has been computed and inverts if not already done
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$returnInverse()
+        if(!is.null(inverse)}{
+                message("Collecting cached data.")
+                return(inverse)
+        }
+        matrix_output <- x$main_mat()
+        inverse <- solve(matrix_output, ...)
+        x$startInverse(inverse)
+        inverse
 }
